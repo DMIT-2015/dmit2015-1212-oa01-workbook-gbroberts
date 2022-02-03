6eb656d9be07fde3916ce87f9970a0a3d6fa15b0
Graham Roberts <groberts14@studentmail.nait.ca>
2022-02-03 16:26:54 -0700

    Finally got JBoss to work.... don't exactly know how. Might've been [INFO] Scanning for projects... [INFO] ------------------------------------------------------------------------ [INFO] BUILD FAILURE [INFO] ------------------------------------------------------------------------ [INFO] Total time:  0.045 s [INFO] Finished at: 2022-02-03T16:26:54-07:00 [INFO] ------------------------------------------------------------------------ [ERROR] The goal you specified requires a project to execute but there is no POM in this directory (/home/user2015/git/dmit2015-1212-oa01-workbook-gbroberts). Please verify you invoked Maven from the correct directory. -> [Help 1] [ERROR] [ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch. [ERROR] Re-run Maven using the -X switch to enable full debug logging. [ERROR] [ERROR] For more information about the errors and possible solutions, please read the following articles: [ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MissingProjectException. I'm just happy it finally works!

--- dmit2015-1212-jaxrs-demo/src/main/java/dmit2015/config/ApplicationConfig.java
+++ dmit2015-1212-jaxrs-demo/src/main/java/dmit2015/config/ApplicationConfig.java
@@ -17,7 +17,7 @@ import jakarta.enterprise.context.ApplicationScoped;
         @DataSourceDefinition(
                 name="java:app/datasources/hsqldatabaseDS",
                 className="org.hsqldb.jdbc.JDBCDataSource",
-		url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-demos-hsqldb",
+                url="jdbc:hsqldb:file:~/jdk/databases/dmit2015-jaxrs-demos-hsqldb;shutdown=true",
 //                url="jdbc:hsqldb:mem:dmit2015hsqldb",
                 user="user2015",
                 password="Password2015"),
