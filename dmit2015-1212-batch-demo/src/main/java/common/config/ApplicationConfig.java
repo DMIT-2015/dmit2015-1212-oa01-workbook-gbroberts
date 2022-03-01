f9afa2aa84d89987816558767cf5ba52a4e44477
Graham Roberts <groberts14@studentmail.nait.ca>
2022-03-01 09:46:44 -0700

    Adds up to end of class. VPN issues preventing wildfly from accessing the server

---
+++ dmit2015-1212-batch-demo/src/main/java/common/config/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package common.config;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.annotation.sql.DataSourceDefinitions;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@DataSourceDefinitions({
+
+	@DataSourceDefinition(
+		name="java:app/datasources/mssqlDS",
+		className="com.microsoft.sqlserver.jdbc.SQLServerDataSource",
+		url="jdbc:sqlserver://DMIT-Capstone1.ad.sast.ca;databaseName=DMIT2015_1212_A01_groberts14;TrustServerCertificate=true",
+		user="groberts14",
+		password="RemoteDesktop200493791"),
+})
+
+@ApplicationScoped
+public class ApplicationConfig {
+
+}
