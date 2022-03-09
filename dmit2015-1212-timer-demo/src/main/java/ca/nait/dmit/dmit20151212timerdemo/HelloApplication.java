de21f116ef8e1ed3f12c27909925c5d37f876290
Graham Roberts <groberts14@studentmail.nait.ca>
2022-03-09 11:07:13 -0700

    Up to break: email service functionality

--- dmit2015-1212-timer-demo/src/main/java/ca/nait/dmit/dmit20151212timerdemo/HelloApplication.java
+++ dmit2015-1212-timer-demo/src/main/java/ca/nait/dmit/dmit20151212timerdemo/HelloApplication.java
@@ -3,7 +3,7 @@ package ca.nait.dmit.dmit20151212timerdemo;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/api")
+@ApplicationPath("/webapi")
 public class HelloApplication extends Application {
 
 }
