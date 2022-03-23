2901ed9d96540d467cb4ae99eacbb958bb66cfcb
Graham Roberts <groberts14@studentmail.nait.ca>
2022-03-23 11:31:58 -0600

    JSF edit/delete/details. Template changes

--- dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
+++ dmit2015-1212-jsf-demo/src/main/java/dmit2015/repository/MovieRepository.java
@@ -5,12 +5,25 @@ import dmit2015.entity.Movie;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.List;
+
 @ApplicationScoped
 @Transactional
 public class MovieRepository extends AbstractJpaRepository<Movie, Long> {
 
     public MovieRepository() {
         super(Movie.class);
+    }
+
+    public List<Movie> listByReleaseDateDescending() {
+        return getEntityManager()
+                .createQuery("""
+                        select m
+                        from Movie m
+                        order by m.releaseDate desc
+                        """
+                        , Movie.class)
+                .getResultList();
     }
 
 }
